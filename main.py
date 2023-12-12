b6c218b17dd5a18f3c9ab1a1407d2a9c2a4a4d92
wolflergf <wolflergf@gmail.com>
2023-12-12 22:03:48 +0000

    Create the routes

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('home.html')
+
+@app.route("/api/v1/<station>/<date>")
+def api(station, date):
+    temperature = 23
+    return {
+        "station": station,
+        "date": date,
+        "temperature": temperature
+    }
 
 if __name__ == '__main__':
     app.run(debug=True)
