1430d15c2ea5877f896346c36ec878003ac874b0
wolflergf <wolflergf@gmail.com>
2023-12-26 00:05:33 +0000

    Show stations table on the homepage

--- main.py
+++ main.py
@@ -1,29 +1,61 @@
+from typing import Dict
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+stations = pd.read_csv("./data_small/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
+
+
+@app.route("/")
+def home() -> str:
+    """
+    Return the home page.
+    """
+    return render_template("home.html", data=stations.to_html())
+
 
 @app.route("/api/v1/<station>/<date>")
-def api(station, date):
-    df = pd.read_csv("./data_small/TG_STAID{}.txt".format(station.zfill(6)), skiprows=20, parse_dates=["    DATE"])
+def api(station: str, date: str) -> Dict[str, str]:
+    """
+    Return the temperature for a given station and date.
+
+    Parameters:
+    station (str): The station ID
+    date (str): The date in YYYYMMDD format
+
+    Returns:
+    dict: A dictionary containing the station ID, date, and temperature
+    """
+    df = pd.read_csv(
+        "./data_small/TG_STAID{}.txt".format(station.zfill(6)),
+        skiprows=20,
+        parse_dates=["    DATE"],
+    )
     temperature = df.loc[df["    DATE"] == date]["   TG"].values[0] / 10
-    return {
-        "station": station,
-        "date": date,
-        "temperature": temperature
-    }
+    return {"station": station, "date": date, "temperature": str(temperature)}
+
 
 @app.route("/api/v1/<definition>/")
-def api_definition(definition):
+def api_definition(definition: str) -> Dict[str, str]:
+    """
+    Return the definition of a given word.
+
+    Parameters:
+    definition (str): The word to define
+
+    Returns:
+    dict: A dictionary containing the word and its definition
+    """
+    word = definition
+    df = pd.read_csv("./dictionary/dictionary.csv", sep=",")
+    definition = df.loc[df["word"] == definition]["definition"].values[0]
     return {
-        "definition": definition.upper(),
-        "word": definition,
+        "definition": definition,
+        "word": word,
     }
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ teste.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("./dictionary/dictionary.csv", sep=",")
+print(df.head())
